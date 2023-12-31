5416ebbf3fab3d8ca2155f3c5f7f9837103e31b5
Himanshu-AT <aristech.official@gmail.com>
2023-12-31 19:16:35 +0530

    Resolve Setup contact page #12

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,35 +9,37 @@ const NavBar = () => {
     setLoggedIn(logginIn);
   }, []);
   return (
-    <div className="flex flex-row justify-between items-center w-full h-16 px-4">
-      <div className="flex flex-row items-center space-x-4 ">
-        <div className="text-2xl font-bold ">
-          <a href="/">Logo</a>
+    <nav className="bg-transparent flex items-center justify-between">
+      <div className="flex flex-row justify-between items-center w-full h-16 px-4">
+        <div className="flex flex-row items-center space-x-4 ">
+          <div className="text-2xl font-bold ">
+            <a href="/">Logo</a>
+          </div>
         </div>
-      </div>
-      {/* <div className="flex flex-row items-center space-x-4 ">
+        {/* <div className="flex flex-row items-center space-x-4 ">
         <a href="/about">About</a>
         <a href="/contact">Contact</a>
       </div> */}
-      <div
-        className="flex flex-row
+        <div
+          className="flex flex-row
                 items-center
                 space-x-4
                 "
-      >
-        {logginIn ? (
-          <>
-            <a href="/dashboard">Profile</a>
-            <a href="/logout">Logout</a>
-          </>
-        ) : (
-          <>
-            <a href="/signin">Login</a>
-            <a href="/signup">Sign Up</a>
-          </>
-        )}
+        >
+          {logginIn ? (
+            <>
+              <a href="/dashboard">Profile</a>
+              <a href="/logout">Logout</a>
+            </>
+          ) : (
+            <>
+              <a href="/signin">Login</a>
+              <a href="/signup">Sign Up</a>
+            </>
+          )}
+        </div>
       </div>
-    </div>
+    </nav>
   );
 };
 
