db74ee8a63873bcdd756431c1990aae8ef6d69a5
oguntunjiharlimatodunola <fimihanodunola625@gmail.com>
2024-01-12 17:58:13 +0100

    Refactor changes

--- next.config.js
+++ next.config.js
@@ -1,27 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [
-            {
-                protocol: 'http',
-                hostname: 'localhost',
-                port: '4000',
-                pathname: '/i/**',
-            },
-            {
-                hostname: `${process.env.NEXT_PUBLIC_SERVER_URL}`,
-                pathname: '/i/**',
-            },
-            {
-                hostname: 'avatars.githubusercontent.com',
-                pathname: '/u/**',
-            },
-            {
-                hostname: 'ai-res-server-development.onrender.com',
-                pathname: '/i/**',
-            }
-        ],
+      remotePatterns: [
+        {
+          protocol: 'http',
+          hostname: 'localhost',
+          port: '4000',
+          pathname: '/i/**',
+        },
+        {
+          hostname: process.env.NEXT_PUBLIC_SERVER_URL,
+          pathname: '/i/**',
+        },
+        {
+          hostname: 'avatars.githubusercontent.com',
+          pathname: '/u/**',
+        },
+        {
+          hostname: 'ai-res-server-development.onrender.com',
+          pathname: '/i/**',
+        }
+      ],
     }
-}
-
-module.exports = nextConfig
+  };
+  
+  export default nextConfig;
+  
